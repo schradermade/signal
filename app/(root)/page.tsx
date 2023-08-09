b354ad1bd600448e1bab7831f659dfce894ed4c3
Nathan Schrader <natecschrader@gmail.com>
2023-08-09 14:38:45 -0700

    add svgs, initial Bar compons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Signal</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+function Leftbar() {
+  return (
+    <>
+      <h1>Leftbar</h1>
+    </>
+  );
+}
+
+export default Leftbar;
