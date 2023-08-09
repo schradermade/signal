204073560c5be8506c6629f9aa7b9168a8138860
Nathan Schrader <natecschrader@gmail.com>
2023-08-09 16:47:29 -0700

    build out Topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,6 +12,30 @@ function Topbar() {
             Signal
           </p>
         </Link>
+        <div className="flex items-center gap-1">
+          <div className="block md:hidden">
+            <SignedIn>
+              <SignOutButton>
+                <div className="flex cursor-pointer">
+                  <Image
+                    src="/assets/logout.svg"
+                    alt="logout"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              </SignOutButton>
+            </SignedIn>
+          </div>
+
+          <OrganizationSwitcher
+            appearance={{
+              elements: {
+                organizationSwitcherTrigger: "py-2 px-4",
+              },
+            }}
+          />
+        </div>
       </nav>
     </>
   );
