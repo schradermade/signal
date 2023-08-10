7db43e963ca2a838a4bcf5833d36ef74bab81e2d
Nathan Schrader <natecschrader@gmail.com>
2023-08-09 23:23:46 -0700

    build out layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,20 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import "../globals.css";
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+// import { dark } from "@clerk/themes";
 
-import Topbar from "@/components/shared/Topbar";
+import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Signal",
-  description: "A Next.js 13 Meta Signal Application",
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
 };
 
 export default function RootLayout({
@@ -21,17 +23,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    // appearance={{
+    //   baseTheme: dark,
+    // }}
+    >
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-          <main>
+
+          <main className="flex flex-row">
             <LeftSidebar />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
+
           <Bottombar />
         </body>
       </html>
