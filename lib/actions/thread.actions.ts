3e7a1165f39545b86401e1edc1a5ae0a046fe153
Nathan Schrader <natecschrader@gmail.com>
2023-08-17 15:45:00 -0700

    add addCommentToThread server action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -103,3 +103,43 @@ export async function fetchThreadById(threadId: string) {
     throw new Error(`Error fetching thread: ${error.mesage}`);
   }
 }
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+
+  try {
+    // adding a comment
+    // find original thread by it's ID
+
+    const originalThread = await Thread.findById(threadId);
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    // Create a new thread with the comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    // save the new thread
+    const savedCommentThread = await commentThread.save();
+
+    // Update original thread to include the new comment
+    originalThread.children.push(savedCommentThread._id);
+
+    // Save original thread
+    await originalThread.save();
+
+    // revalidate so it shows instantly
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error adding comment to thread: ${error.message}`);
+  }
+}
