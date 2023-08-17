ab894c220e23ae86939f1da62e8f5a7f992c3714
Nathan Schrader <natecschrader@gmail.com>
2023-08-17 14:40:24 -0700

    add isComment check

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
     <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
@@ -90,6 +91,13 @@ const ThreadCard = ({
                   height={24}
                 />
               </div>
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
