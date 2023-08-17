888e4e9bece47dc6c4bd1fc4e51b3f7bf588f6fe
Nathan Schrader <natecschrader@gmail.com>
2023-08-17 15:08:34 -0700

    add Comment form to ThreadCard

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,18 +1,21 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Comment from "@/components/forms/Comment";
 
-const Page = async ({ params }: { params: { id: string } }) => {
+const page = async ({ params }: { params: { id: string } }) => {
   if (!params.id) return null;
 
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/onboarding");
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const thread = await fetchThreadById(params.id);
+  console.log("THREAD:", thread);
 
   return (
     <section className="relative">
@@ -29,8 +32,15 @@ const Page = async ({ params }: { params: { id: string } }) => {
           comments={thread.children}
         />
       </div>
+      <div className="mt-7">
+        <Comment
+          threadId={thread.id}
+          currentUserImg={user.imageUrl}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
     </section>
   );
 };
 
-export default Page;
+export default page;
