18905fe95e70e941cd875f17cd2f4138a05692d0
Nathan Schrader <natecschrader@gmail.com>
2023-08-18 10:43:56 -0700

    add skeleton of Threads tab

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  // TODO: Fetch profile threads
+
+  return <section>ThreadsTab</section>;
+};
+
+export default ThreadsTab;
