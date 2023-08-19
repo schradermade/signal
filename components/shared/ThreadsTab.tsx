78a01a4dc4f242a5cefe0e064c9415d56840d50f
Nathan Schrader <natecschrader@gmail.com>
2023-08-18 21:09:57 -0700

    build out ThreadsTab comp with ThreadCard

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,3 +1,8 @@
+import { fetchUserPosts } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import ThreadCard from "../cards/ThreadCard";
+import { currentUser } from "@clerk/nextjs";
+
 interface Props {
   currentUserId: string;
   accountId: string;
@@ -5,9 +10,37 @@ interface Props {
 }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-  // TODO: Fetch profile threads
+  const user = await currentUser();
 
-  return <section>ThreadsTab</section>;
+  // TODO: Fetch profile threads
+  let result = await fetchUserPosts(accountId);
+  if (!result) redirect("/");
+
+  return (
+    <section className="mt-9 flex flex-col gap-10">
+      {result.threads.map((thread: any) => (
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={user?.id || ""}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={
+            accountType === "User"
+              ? { name: result.name, image: result.image, id: result.id }
+              : {
+                  name: thread.author.name,
+                  image: thread.author.image,
+                  id: thread.author.id,
+                }
+          }
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default ThreadsTab;
