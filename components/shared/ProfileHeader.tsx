624af6b458c79764441f465a6db985c8e79fff83
Nathan Schrader <natecschrader@gmail.com>
2023-08-18 21:09:32 -0700

    fix small styling error

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -40,7 +40,7 @@ const ProfileHeader = ({
 
       {/* TODO: Community */}
       <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
-      <div className="mt 12 h-0.5 w-full bg-dark-3" />
+      <div className="mt-12 h-0.5 w-full bg-dark-3" />
     </div>
   );
 };
